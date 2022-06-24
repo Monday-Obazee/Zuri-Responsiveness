e6c8cb33f4833737a98edc88dcc45fa0c2f1cd4d
Monday-Obazee <mondayobazee@gmail.com>
2022-06-24 15:21:42 +0100

    active state edited

--- main.js
+++ main.js
@@ -1,19 +1,22 @@
-const now = document.querySelectorAll(".p");
-const button = document.querySelector(".btn1");
-let firstForm = document.querySelectorAll("#firstForm");
-const formNow = document.querySelector("#form1");
-function fun() {
-  for (let i = 0; i < now.length; i++) {
-    let currentEl = now[i];
-    currentEl.style.display = "none";
-  }
-}
+// const now = document.querySelectorAll(".p");
+// const button = document.querySelector(".btn1");
+// let firstForm = document.querySelectorAll("#firstForm");
+// const formNow = document.querySelector("#form1");
+// function fun() {
+//   for (let i = 0; i < now.length; i++) {
+//     let currentEl = now[i];
+//     currentEl.style.display = "none";
+//   }
+// }
+// function checkInput() {
+//   const firstInput = firstForm.value.trim();
+// }
 
-fun();
-
-formNow.addEventListener("submit", (e) => {
-  if (firstForm.value === "" || firstForm == null) {
-    now.style.display = "block";
-  }
-  return false;
-});
+// formNow.addEventListener("submit", (e) => {
+//   e.preventDefault();
+//   if (firstInput.value === "") {
+//     currentEl.style.display = "block";
+//   }
+//   return false;
+// });
+// fun();
